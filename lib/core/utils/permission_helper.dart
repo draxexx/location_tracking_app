a8f79ce613d15d1e49bb5a39e7b76f9add93f880
Burak <burak.yilmaz@simdatix.com>
2025-03-22 21:25:18 +0300

    fix permission

--- lib/core/utils/permission_helper.dart
+++ lib/core/utils/permission_helper.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:location_tracking_app/core/init/application_initialize.dart';
 import 'package:location_tracking_app/core/utils/consts/enums/location_permission_status.dart';
 import 'package:location_tracking_app/core/widgets/confirm_dialog.dart';
@@ -36,7 +37,16 @@ final class PermissionHelper {
       return false;
     } else if (permissionStatus == LocationPermissionStatus.grantedWhileInUse) {
       if (Platform.isAndroid) {
-        await geolocatorService.requestPermission();
+        final permission = await geolocatorService.requestPermission();
+
+        if (permission == LocationPermission.whileInUse && context.mounted) {
+          showConfirmDialog(
+            context: context,
+            onSubmit: () => geolocatorService.openSettings(),
+            message:
+                "Please enable location permission as 'Always' to continue.",
+          );
+        }
       } else if (Platform.isIOS) {
         showConfirmDialog(
           context: context,
