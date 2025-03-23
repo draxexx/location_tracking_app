fbe75691ece5a047240ac61d4aa073c038f467e8
Burak <burak.yilmaz@simdatix.com>
2025-03-23 17:20:26 +0300

    add some somments on extensions

--- lib/core/utils/extensions/datetime_extensions.dart
+++ lib/core/utils/extensions/datetime_extensions.dart
@@ -1,12 +1,14 @@
 import 'package:intl/intl.dart';
 
 extension DatetimeExtensions on DateTime {
+  /// Formats the date to a human readable string.
   String formatDate() {
     String formattedDate = DateFormat('yyyy-MM-dd').format(this);
 
     return formattedDate;
   }
 
+  /// Checks if the date is the same as the other date.
   bool isSameDay(DateTime other) {
     return year == other.year && month == other.month && day == other.day;
   }
